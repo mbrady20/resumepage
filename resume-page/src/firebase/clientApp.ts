82e9f08ada034dccdd846c72e5d23ffb83f80ffd
mbrady20 <bradymichael362@gmail.com>
2023-08-30 15:38:17 -0500

    added firebase

---
+++ resume-page/src/firebase/clientApp.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCauZo-imU-pJk8VJPxsJRJXoOwkWJAAAg",
+  authDomain: "myresumepage-42803.firebaseapp.com",
+  projectId: "myresumepage-42803",
+  storageBucket: "myresumepage-42803.appspot.com",
+  messagingSenderId: "354577582024",
+  appId: "1:354577582024:web:2253ef0ccc3ce4e1cd22fd"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
